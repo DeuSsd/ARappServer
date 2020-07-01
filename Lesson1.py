b8c12fb5edbed53d41e4224e14b99dd221dec939
Ilya Golovakov <golovakov.ilya@gmail.com>
2020-07-01 17:57:19 +0300

    my proga

---
+++ Lesson1.py
@@ -0,0 +1,14 @@
+#login=input("Enter login: ")
+#password=input("Enter password: ")
+#a=DB.getNamefromlogin(login, password)
+#
+#if a=="true":
+#    print("Successful login and password")
+#else:
+#    print("Incorrect login or password")
+
+
+
+
+
+
