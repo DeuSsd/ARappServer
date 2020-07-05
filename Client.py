92d0372dcf3a4bf8f036ba2d0b28a4c1a9ed958e
Denis Ognev <den.is.ognev@yandex.ru>
2020-07-05 17:29:36 +0200

    Prepare library for handle of requests

--- Client.py
+++ Client.py
@@ -45,25 +45,9 @@ msg4 = {
     }
 }
 
+msg = [msg1, msg2, msg3, msg4, msg5]
 
-def getLength(Socket):
-    length = 0
-    while not length:
-        length = int(Socket.recv(1024).decode())
-    Socket.sendall(bytes(length))
-    return length
-
-def setLength(Socket,msg):
-    Socket.sendall(bytes(len(msg)))
-    length = int(Socket.recv(1024).decode())
-    while not len(msg) == length:
-        length = int(Socket.recv(1024).decode())
-
-    return length
-msg = [msg1,msg2,msg3,msg4,msg5]
-
-HOST, PORT = "25.79.246.93", 50000
-# HOST, PORT = "192.168.1.105", 50000
+HOST, PORT = "192.168.1.105", 50000
 # Create a socket (SOCK_STREAM means a TCP socket)
 for dataMsg in msg:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -83,41 +67,3 @@ for dataMsg in msg:
         print("Received: {}".format(received))
         print("------")
 
-
-
-
-
-
-
-
-# import multiprocessing
-# import multiprocessing.connection as connection
-
-# def producer(data, address, authkey):
-#     with connection.Listener(address, authkey=authkey) as listener:
-#         with listener.accept() as conn:
-#             print('connection accepted from', listener.last_accepted)
-#             for item in data:
-#                 print("producer sending:", repr(item))
-#                 conn.send(item)
-#
-# def consumer(address, authkey):
-#     with connection.Client(address, authkey=authkey) as conn:
-#         try:
-#             while True:
-#                 item = conn.recv()
-#                 print("consumer received:", repr(item))
-#         except EOFError:
-#             pass
-#
-# listen_address = "localhost", 50000
-# remote_address = "localhost", 50000
-# authkey = b'secret password'
-#
-# if __name__ == "__main__":
-#     data = ["1", "23", "456"]
-#     p = multiprocessing.Process(target=consumer, args=(data, listen_address, authkey))
-#     p.start()
-#     consumer(remote_address, authkey)
-#     p.join()
-#     print("done")
