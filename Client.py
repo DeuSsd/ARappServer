f432caf13c60230c138f2a59112abfae5121caaf
Denis Ognev <den.is.ognev@yandex.ru>
2020-07-13 00:02:23 +0200

    return by add handle an generator

--- Client.py
+++ Client.py
@@ -1,130 +1,53 @@
 import socket
 import time
 
-#
-# def client(ip, port, message):
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     sock.connect((ip, port))
-#     try:
-#         while True:
-#             time.sleep(5)
-#             sock.sendall(message)
-#             response = sock.recv(1024)
-#             print(")Received: {}".format(response))
-#     finally:
-#         sock.close()
-#
-#
-# if __name__ == '__main__':
-#     client(*('127.0.0.1', 56075), "Hello World 1")
+msg1 = {
+    "method": "get",
+    "parametrs": {
+        "collectionName": "radiator",
+        "filter": {
+            "Temperature": {'$lt': 50}
+        }
+    }
+}
 
-# # Import socket module
-# import socket
-#
-#
-# def Main():
-#     # local host IP '127.0.0.1'
-#     host = '127.0.0.1'
-#
-#     # Define the port on which you want to connect
-#     port = 12345
-#
-#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-#     # connect to server on local computer
-#     s.connect((host, port))
-#
-#     # message you send to server
-#     message = "shaurya says geeksforgeeks"
-#     while True:
-#
-#         # message sent to server
-#         s.send(message.encode('ascii'))
-#
-#         # messaga received from server
-#         data = s.recv(1024)
-#
-#         # print the received message
-#         # here it would be a reverse of sent message
-#         print('Received from the server :', str(data.decode('ascii')))
-#
-#         # ask the client whether he wants to continue
-#         ans = input('\nDo you want to continue(y/n) :')
-#         if ans == 'y':
-#             continue
-#         else:
-#             break
-#     # close the connection
-#     s.close()
-#
-#
-# if __name__ == '__main__':
-#     Main()
-#
-# # import socket
-# # import time
-# #
-# # msg1 = {
-# #     "method": "get",
-# #     "parametrs": {
-# #         "collectionName": "radiator",
-# #         "filter": {
-# #             "Temperature": {'$gt': 95}
-# #         }
-# #     }
-# # }
-# #
-# # msg2 = {
-# #     "method": "delete",
-# #     "parametrs": {
-# #         "collectionName": "radiator",
-# #         "filter": {
-# #             "Temperature": {'$lt': 10}
-# #         }
-# #     }
-# # }
-# #
-# # msg3 = {
-# #     "method": "put",
-# #     "parametrs": {
-# #         "collectionName": "radiator",
-# #         "data": {
-# #             "Temperature": 150.256
-# #         }
-# #     }
-# # }
-# #
-# # msg5 = {
-# #     "method": "put"
-# # }
-# #
-# # msg4 = {
-# #     "method": "get",
-# #     "parametrs": {
-# #         "collectionName": "radiator",
-# #         "filter": {
-# #             "Temperature": {'$gt': 150}
-# #         }
-# #     }
-# # }
-# #
-# #
-# # def getLength(Socket):
-# #     length = 0
-# #     while not length:
-# #         length = int(Socket.recv(1024).decode())
-# #     Socket.sendall(bytes(length))
-# #     return length
-# #
-# # def setLength(Socket,msg):
-# #     Socket.sendall(bytes(len(msg)))
-# #     length = int(Socket.recv(1024).decode())
-# #     while not len(msg) == length:
-# #         length = int(Socket.recv(1024).decode())
-# #
-# #     return length
-# # msg = [msg1,msg2,msg3,msg4,msg5]
-# # #getlockal ip
+msg2 = {
+    "method": "delete",
+    "parametrs": {
+        "collectionName": "radiator",
+        "filter": {
+            "Temperature": {'$lt': 10}
+        }
+    }
+}
+
+msg3 = {
+    "method": "put",
+    "parametrs": {
+        "collectionName": "radiator",
+        "data": {
+            "Temperature": 150.256
+        }
+    }
+}
+
+msg5 = {
+    "method": "put"
+}
+
+msg4 = {
+    "method": "get",
+    "parametrs": {
+        "collectionName": "radiator",
+        "filter": {
+            "Temperature": {'$gt': 150}
+        }
+    }
+}
+
+
+msg = [msg1,msg2,msg3]#,msg4#,msg5]
+#getlockal ip
 # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # s.connect(("8.8.8.8", 80))
 # print(s.getsockname()[0])
@@ -134,65 +57,21 @@ HOST, PORT = "localhost", 9999
 # HOST, PORT = "25.79.246.93", 50000
 # HOST, PORT = "localhost", 40000
 # Create a socket (SOCK_STREAM means a TCP socket)
-# for dataMsg in msg:
+# msg = ["sd"]
 while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        # Connect to server and send data
-        # while True:
-        sock.connect((HOST, PORT))
-        # socket.sendall(data)
-        # Receive data from the server and shut down
-        # length = int(sock.recv(1024).decode())
-        # setLength(socket,data)
-
-        dataMsg = "asda"
-        # time.sleep(0.001)
-        data = str(dataMsg).encode()
-        sock.sendall(data)
-        # lenght = getLength()
-        lenght = 4096
-        received = sock.recv(lenght)
-        print("Sent:     {}".format(data))
-        print("Received: {}".format(received))
-        print("------")
-        time.sleep(1)
-        sock.close()
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# # # import multiprocessing
-# # # import multiprocessing.connection as connection
-# #
-# # # def producer(data, address, authkey):
-# # #     with connection.Listener(address, authkey=authkey) as listener:
-# # #         with listener.accept() as conn:
-# # #             print('connection accepted from', listener.last_accepted)
-# # #             for item in data:
-# # #                 print("producer sending:", repr(item))
-# # #                 conn.send(item)
-# # #
-# # # def consumer(address, authkey):
-# # #     with connection.Client(address, authkey=authkey) as conn:
-# # #         try:
-# # #             while True:
-# # #                 item = conn.recv()
-# # #                 print("consumer received:", repr(item))
-# # #         except EOFError:
-# # #             pass
-# # #
-# # # listen_address = "localhost", 50000
-# # # remote_address = "localhost", 50000
-# # # authkey = b'secret password'
-# # #
-# # # if __name__ == "__main__":
-# # #     data = ["1", "23", "456"]
-# # #     p = multiprocessing.Process(target=consumer, args=(data, listen_address, authkey))
-# # #     p.start()
-# # #     consumer(remote_address, authkey)
-# # #     p.join()
-# # #     print("done")
+    for dataMsg in msg:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Connect to server and send data
+            # while True:
+            sock.connect((HOST, PORT))
+            # socket.sendall(data)
+            # Receive data from the server and shut down
+            data = str(dataMsg).encode()
+            sock.sendall(data)
+            lenght = 20480
+            received = sock.recv(lenght)
+            # print("Sent:     {}".format(data))
+            # print("Received: {}".format(received))
+            # print("------")
+            time.sleep(0.01)
+            sock.close()
--- ServerApp.py
+++ ServerApp.py
@@ -1,64 +1,7 @@
-# import socketserver
-# import socket
-# import handlerJSON
-# import asynio
-#
-# class MyTCPHandler(socketserver.BaseRequestHandler):
-#
-#     def handle(self):
-#         # lenght = self.getLength(self.request)
-#         lenght = 10240
-#         self.data = self.request.recv(lenght).decode()  # .strip()
-#         print("------------\nClient address: {}:{}\nRequest: {}".format(*self.client_address, self.data))
-#         # print("{} wrote:".format(self.client_address[0]))
-#         # print(pickle.loads(self.data))
-#         # print(self.data)
-#         # socket.sendto(data, self.client_address)
-#         responseMsg = handlerJSON.loadMessage(self.data)
-#         print("Responce: {}".format(responseMsg))
-#         msg = str(responseMsg).encode()
-#         # self.setLength(self.request,msg)
-#         self.request.sendall(msg)
-#
-#     # def getLength(self,Socket):
-#     #     length = 0
-#     #     while not length:
-#     #         length = int(Socket.recv(1024).decode())
-#     #     Socket.sendall(bytes(length))
-#     #     return length
-#     #
-#     # def setLength(self,Socket, msg):
-#     #     Socket.sendall(len(msg))
-#     #     length = int(Socket.recv(1024).decode())
-#     #     if len(msg) == length:
-#     #         return length
-#
-#
-#
-# if __name__ == "__main__":
-#     #getlockal ip
-#     HOST, PORT = 'localhost', 9999
-#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     s.connect(("8.8.8.8", 80))
-#     # print(s.getsockname()[0])
-#     # HOST, PORT = "25.79.246.93", 9090
-#     # HOST, PORT = s.getsockname()[0], 50000
-#     s.close()
-#     print(HOST)
-#     # Create the server, binding to localhost on port 9999
-#     with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-#         # Activate the server; this will keep running until you
-#         # interrupt the program with Ctrl-C
-#         server.serve_forever()
-#         server.server_close()
-#
-
-
-
-
 import socket
 from select import select
 from server import handlerJSON
+
 
 HOST, PORT = 'localhost', 9999
 
@@ -76,21 +19,22 @@ def server():
     while True:
         yield ('read', server_socket)
         client_socket, addr = server_socket.accept()
-        print('Connection from ', addr)
+        # print('Connection from ', addr)
         tasks.append(client(client_socket))
 
 def client(client_socket):
     while True:
         yield ('read', client_socket)
         request = client_socket.recv(4096)
-
         # print(request)
         if not request:
             break
         else:
-            response = handlerJSON.loadMessage(request.decode())
+            # print("------------\nClient address: {}:{}\nRequest: {}".format(*client_socket.getpeername(), request.decode()))
+            response = handlerJSON.loadMessage(request.decode()) #block process
             msg =str(response).encode()
             yield ('write',client_socket)
+            # print("Responce: {}".format(msg.decode()))
             client_socket.send(msg)
     client_socket.close()
 
